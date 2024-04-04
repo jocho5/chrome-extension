328da9995e5760f4cd699f5b9f091da363afa723
Joe Cho <hyongscho@gmail.com>
2024-04-04 09:25:18 -0700

    Chrome-Ext-MVP

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.declarativeNetRequest.onRequest.addListener(
-    function(details) {return {cancel: true}},
-    {urls: ["*://.kevel.com/*"]},
-    ["blocking"]
-)
---
+++ main.js
@@ -0,0 +1,16 @@
+// below may resemble response from API GET request? Note > I need to understand CORS and how to hide/secure API-keys (e.g., RAPID API)
+const jokesArr = [
+    { joke: "All arrays Chuck Norris declares are of infinite size, because Chuck Norris knows no bounds." },
+    { joke: "Chuck Norris writes code that optimizes itself." },
+    { joke: "Chuck Norris doesn’t need garbage collection because he doesn’t call .Dispose(), he calls .DropKick()." },
+    { joke: "Chuck Norris can write infinite recursion functions…and have them return." },
+    { joke: "Chuck Norris can delete the Recycling Bin." },
+    { joke: "Chuck Norris doesn't make http requests. He makes http demands." }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const randomJoke = Math.floor(Math.random() * jokesArr.length);
+
+    document.getElementById('joke').textContent = jokesArr[randomJoke].joke;
+
+})
