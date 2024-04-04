2caee690dc7b1e9bdc6b815780d8204446f5aa5e
Joe Cho <hyongscho@gmail.com>
2024-04-03 21:51:17 -0700

    Chrome-Ext-WIP

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('This is a popup!');
+alert('This is a popup!');
+
+// boilerplate?
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.action.setBadgeText({
+//       text: 'OFF'
+//     });
+//   });
+
+
+
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: {tabId: tab.id},
+//     files: ['content.js']
+//   });
+// });
